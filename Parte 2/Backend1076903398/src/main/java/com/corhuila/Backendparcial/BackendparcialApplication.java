1b9338c030779aaf6905bcd8fadd0f4ccbeac0f1
NicolasAlvarez25 <ngalvarez-2022b@corhuila.edu.co>
2024-10-08 23:27:02 -0500

    prueba

---
+++ Parte 2/Backend1076903398/src/main/java/com/corhuila/Backendparcial/BackendparcialApplication.java
@@ -0,0 +1,13 @@
+package com.corhuila.Backendparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendparcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendparcialApplication.class, args);
+	}
+
+}
---
+++ Parte 2/Backend1076903398/src/test/java/com/corhuila/Backendparcial/BackendparcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corhuila.Backendparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendparcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
